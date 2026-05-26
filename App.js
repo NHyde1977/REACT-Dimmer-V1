c5cca1b6e8ece7199d99e51e49971613b8fc2539
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2026-05-26 19:22:36 -0300

    1ª versão  app Dimmer

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+ 
+const [ligada, setLigada] = useState(true);
+
+function  ligar() {
+  setLigada(true);
+}
+
+function desligar() {
+  setLigada(false);
+}
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {backgroundColor: ligada ? '#fff176' : '#2c3e50'}]}>
+      <Text style={[styles.mensagem, {color: ligada ? '#000' : '#fff'}]}>
+        {ligada ? 'A lâmpada está LIGADA 💡' : 'A lâmpada está DESLIGADA 🌑'}
+      </Text>
+      <TouchableOpacity style={styles.botao} onPress={ligada? desligar : ligar}>
+        <Text style={styles.texoBotao}>
+          {ligada ? 'Apagar' : 'Acender'}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,6 +33,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+
+  mensagem: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold'
+  },
+
+  botao:{
+    backgroundColor: '#2196f3',
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+
+  texoBotao: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 });
